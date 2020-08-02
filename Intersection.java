e11f329f724ad42ac414c404674baf380d06975b
Ivor <ivor.benderavage@gmail.com>
2020-08-01 22:41:07 -0400

    Initial commit

---
+++ Intersection.java
@@ -0,0 +1,19 @@
+public class Intersection {
+
+    enum Type {
+        TWO_WAY,
+        THREE_WAY,
+        FOUR_WAY
+    }
+
+    Type intersectionType;
+
+    // Constructor
+    public Intersection(Type type) { 
+        intersectionType = type;
+    }
+
+
+
+
+}
---
+++ Traffic.java
@@ -0,0 +1,17 @@
+public class Traffic {
+    
+
+    private static final Intersection intersection = new Intersection(Intersection.Type.FOUR_WAY);
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world!");
+        System.out.println("Haven't used Java in a while");
+
+    }
+
+
+
+
+
+}
